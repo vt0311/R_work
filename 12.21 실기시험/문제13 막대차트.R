621c05d76b2941f945a9c5536e05b0c285ba7e97
swhah <hsw0311@nate.com>
2017-12-22 16:23:16 +0900

    fff

--- 12.21 실기시험/문제13 막대차트.R
+++ 12.21 실기시험/문제13 막대차트.R
@@ -9,4 +9,5 @@ head(mydata)
 mytable2 <- table(mydata$이름)
 mytable2
 myframe <- sort(mytable2, decreasing = TRUE)
+windows()
 barplot(myframe, xlab = '회원 이름', ylab = '게시물 건수', main = '회원별 게시물 작성 건수', col = rainbow(4))
